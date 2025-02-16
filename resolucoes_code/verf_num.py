f564a0e28782b5539a71d8856b3d2c21e80ef90e
Mayra Oishi <namie.oishi@gmail.com>
2025-02-16 15:07:07 -0300

    Correções

--- resolucoes_code/verf_num.py
+++ resolucoes_code/verf_num.py
@@ -1,7 +1,7 @@
-num = int(input("Digite um número inteiro))
+num = int(input("Digite um número inteiro"))
 
-if (num%2 = 0)
+if num % 2 == 0:
   print("O número é par")
-else 
+else:
   print("O número é ímpar")
                   
