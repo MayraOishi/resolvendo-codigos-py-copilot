137dea787ef457ce6456293ffa5de6c17aa80284
Mayra Oishi <namie.oishi@gmail.com>
2025-02-16 14:44:36 -0300

    Concatenando

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,8 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite outra informação: ")
+
+info_concatenada = info1 + " " + info2
+
+print("As informações concatenadas são: " + info_concatenada)
