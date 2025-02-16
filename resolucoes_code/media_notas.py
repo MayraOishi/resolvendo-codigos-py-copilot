7ea66b32b62188e7cff06e22c1a3410905fb581c
Mayra Oishi <namie.oishi@gmail.com>
2025-02-16 15:15:42 -0300

    Resolvendo a média

---
+++ resolucoes_code/media_notas.py
@@ -0,0 +1,7 @@
+nota1 = float(input("Qual foi a primeira nota? "))
+nota2 = float(input("Qual foi a segunda nota? "))
+nota3 = float(input("Qual foi a terceira nota? "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+print("A média das notas é " + str(media))
