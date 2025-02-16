6fae50b03df3591782bdddd6d0bf52f4322c00e1
Mayra Oishi <namie.oishi@gmail.com>
2025-02-16 14:49:20 -0300

    Soma de dois números

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,8 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+num1 = int(input("Digite um número: ")
+num2 = int(input("Digite outro número: ")
+
+soma = num1 + num2
+
+print("A soma entre " + num1 + " + " + num2 + "é igual a " + soma")
