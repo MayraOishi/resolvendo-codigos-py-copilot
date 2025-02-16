d3f4a16412d09b8a376e62f43130aede9281a139
Mayra Oishi <namie.oishi@gmail.com>
2025-02-16 14:59:07 -0300

    Colocando a possibilidade de fazer várias operações

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -3,6 +3,15 @@
 num1 = int(input("Digite um número: ")
 num2 = int(input("Digite outro número: ")
 
-soma = num1 + num2
+operacao = input("Digite a operação que deseja realizar: ")
 
-print("A soma entre " + num1 + " + " + num2 + "é igual a " + soma")
+if operacao == "+":
+  print(num1 + num2) 
+elif operacao == "-":
+  print(num1 - num2) 
+elif operacao == "*":
+  print(num1 * num2) 
+elif operacao == "/":
+  print(num1 / num2) 
+else 
+  print("Operação inválida")
